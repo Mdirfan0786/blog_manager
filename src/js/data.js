20a811cd587a2cf0211bd6b3adfa40f2bd98c8f6
Md Irfan <rn555666777@gmail.com>
2026-04-24 15:00:24 +0530

    feat: Vite setup, blog CRUD logic, dynamic rendering, responsive UI, semantic HTML

---
+++ src/js/data.js
@@ -0,0 +1,14 @@
+// src/js/data.js
+let blogs = [];
+
+export function getBlogs() {
+  return blogs;
+}
+
+export function addBlog(blog) {
+  blogs.push(blog);
+}
+
+export function deleteBlog(id) {
+  blogs = blogs.filter((blog) => blog.id !== id);
+}
---
+++ src/js/ui.js
@@ -0,0 +1,33 @@
+// src/js/ui.js
+import { getBlogs, deleteBlog } from "./data.js";
+
+export function renderBlogList(container) {
+  container.innerHTML = "";
+
+  getBlogs().forEach((blog) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <div class="list_content">
+        <h3>${blog.title}</h3>
+        <p>${blog.body}</p>
+      </div>
+      
+      <div class="post_btns">
+        <button class="edit_btn" data-id="${blog.id}">Edit</button>
+        <button class="delete-btn" data-id="${blog.id}">Delete</button>
+      </div>
+    `;
+
+    container.appendChild(li);
+  });
+}
+
+export function bindDeleteEvents(container, onDelete) {
+  container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete-btn")) {
+      const id = parseInt(e.target.dataset.id);
+      onDelete(id);
+    }
+  });
+}
